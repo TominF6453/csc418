2b46e8f87c463f75c9ac6a3c20dc0585c26d6ed0
Filip Tomin <tominf6453@gmail.com>
2018-03-30 17:35:09 -0400

    light_source.cpp complete

--- A3/raytracer/light_source.cpp
+++ A3/raytracer/light_source.cpp
@@ -17,24 +17,31 @@ void PointLight::shade(Ray3D& ray) {
 	// It is assumed at this point that the intersection information in ray 
 	// is available.  So be sure that traverseScene() is called on the ray 
 	// before this function.  
-    Vector3D norm = ray.intersection.normal;
     
-    Vector3D rayToEyeDir = -ray.dir;
-    
-    //get light dir, from PointLight pos to intersection pt
-    Vector3D lightDir = pos - ray.intersection.point;
-    lightDir.normalize(); //normalize the light dir
-    
-    //calculate reflection dir
-    Vector3D reflec = -lightDir.dot(normal);
-    reflec.normalize;
-    
-    Material *intersMat = ray.intersection.mat;
-    Color amb = (*intersMat).ambient * col_ambient;
-    Color diff = (*intersMat).diffuse * (fmax(0.0, normal.dot(lightDir))) * col_diffuse;
-    Color spec = (*intersMat).specular * (fmax(0.0, pow(rayToEyeDir.dot(reflec), (*intersMat).specular_exp))) * col_specular;
-    
-    ray.col = amb + diff + spec;
-    ray.col.clamp();
+	// Normalized vectors for phong shading (from A2's phong shader)
+	Vector3D n = ray.intersection.normal; // norm
+	n.normalize();
+	Vector3D l = pos-ray.intersection.point; // lightdir
+	l.normalize();
+	Vector3D v = -ray.dir; // view
+	v.normalize();
+	Vector3D r = 2.0*(l.dot(n)*n)-l; // reflec
+	r.normalize();
+	
+	// Ambient Lighting
+	Color Ia = (*ray.intersection.mat).ambient * col_ambient;
+	
+	// Diffuse Lighting
+	float dotprod = fmax(0.0, l.dot(n));
+	Color Id = dotprod * (*ray.intersection.mat).diffuse * col_diffuse;
+	
+	// Specular Lighting
+	dotprod = fmax(0.0, r.dot(v));
+	Color Is = pow(dotprod, (*ray.intersection.mat).specular_exp) * (*ray.intersection.mat).specular
+					* col_specular;
+	
+	// Set color
+	ray.col = Ia + Id + Is;
+	ray.col.clamp();
 }
 
